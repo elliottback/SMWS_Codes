adbf4fe15efe16519dff94c10e89b2818b9bd736
elliottback <elliottback@gmail.com>
2021-10-23 18:42:36 +0900

    fix: sort out npm

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "production",
-    devtool: "inline-source-map",
+    devtool: false,
 
     output: {
         path: path.resolve(__dirname, 'dist'),
